1ef1820ff4012fced8156747075156246b10f646
eathren <nolan.braman@gmail.com>
2019-11-30 00:00:42 -0500

    line spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,20 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className="page-home">
       <div className="image-wrapper-home">
-        <img className="image-home-tree" style={{ fill: "red" }} src={Tree} />
+        <img className="image-home-tree" src={Tree} />
         <div className="image-wrapper-home-card">
           <div className=".image-wrapper-home-card-text">
-            <h1>Nolan Braman</h1>
-            <h1> Software Developer</h1>
+            <p>Nolan Braman</p>
+            <p> Software Developer</p>
+            <Link className="page-home-link" to="/about">
+              About
+            </Link>
+            <Link className="page-home-link" to="/portfolio">
+              Portfolio
+            </Link>
+            <Link className="page-home-link" to="/pricing">
+              Pricing
+            </Link>
           </div>
         </div>
       </div>
