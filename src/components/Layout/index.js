b31f4476a04b3a3c6726f434c612b3db72d1a0f0
eathren <nolan.braman@gmail.com>
2019-11-29 21:54:11 -0500

    first commit

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -9,8 +9,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
+// import Header from "../Header"
 import "./layout.css"
+import "../../styles/main.scss"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,24 +25,17 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+    <React.StrictMode>
+      {/* <Header siteTitle={data.site.siteMetadata.title} /> */}
+      <div>
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          {/* © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+                    <a href="https://www.gatsbyjs.org">Gatsby</a> */}
         </footer>
       </div>
-    </>
+    </React.StrictMode>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-
+import Tree from "../images/tree.svg"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="page-home">
+      <div className="image-wrapper-home">
+        <img className="image-home-tree" style={{ fill: "red" }} src={Tree} />
+        <div className="image-wrapper-home-text">
+          <h1>Nolan Braman</h1>
+          <h1> Software Developer</h1>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
