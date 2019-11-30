85fc1f730e812c1d160a1ded99c63a648d68f10a
eathren <nolan.braman@gmail.com>
2019-11-29 22:00:02 -0500

    tree updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,11 @@ const IndexPage = () => (
     <div className="page-home">
       <div className="image-wrapper-home">
         <img className="image-home-tree" style={{ fill: "red" }} src={Tree} />
-        <div className="image-wrapper-home-text">
-          <h1>Nolan Braman</h1>
-          <h1> Software Developer</h1>
+        <div className="image-wrapper-home-card">
+          <div className=".image-wrapper-home-card-text">
+            <h1>Nolan Braman</h1>
+            <h1> Software Developer</h1>
+          </div>
         </div>
       </div>
     </div>
