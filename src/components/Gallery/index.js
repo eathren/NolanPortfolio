d7a57a165f80e993a49f28298088058e23f90902
nolan braman <nolan.braman@gmail.com>
2019-12-09 21:37:56 -0500

    dev commit 1 experimental

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -2,6 +2,10 @@ import React from "react"
 import { Link } from "gatsby"
 import ButtonGroup from "@material-ui/core/ButtonGroup"
 import Button from "@material-ui/core/Button"
+import TestImage from "../../components/image"
+import Image from "../../components/image"
+
+import Grid from "@material-ui/core/Grid"
 
 const items = [
   {
@@ -11,6 +15,7 @@ const items = [
       " A subscription  CME website, IARS360 is an new and improved learning and technical resource website for an better experience in Continued Medical Education for healthcare professionals. ",
     // img: [iars2],
     // iars2,
+    img: <TestImage />,
     webstack:
       "Webstack: Gatsby.js, React.js, GraphQL,   Netlify, Material-UI, and Algolia.",
     link: "",
@@ -21,6 +26,7 @@ const items = [
     description:
       "One of the largest employers in Alaska, ADC wanted a fresh face for their construction and renovation company, in a static Gatsby format to make updating job postings and recent work easy to navigate and update.",
     // img: [adc2],
+    img: <TestImage />,
     webstack: "Gatsby.js, React.js, Netlify, Material-UI",
     link: "https://adcalaska.com",
   },
@@ -30,6 +36,7 @@ const items = [
     description:
       "A hybrid webapp combining the best of static and dynamic use cases for Program Directors to monitor their students and anesthesiology medical students.",
     img: "",
+    img: <TestImage />,
     webstack: "Gatsby.js, React.js, Netlify, Material-UI, GraphQL, MongoDB",
     link: "",
   },
@@ -39,6 +46,7 @@ const items = [
     description:
       "Built with Gatsby and GraphQL, the new Keywords app contains Open Anesthetic's massive list of medical study keywords for their Continuing Medical Education(CME) content, with over 6000 full page entries, sortable by category, year, and difficulty,   ",
     // img: [keywords3, keywords4],
+    img: <TestImage />,
     webstack:
       "Webstack: Gatsby.js, React.js, GraphQL, MongoDB/MongoStitch, Node.js,  Netlify, Material-UI, and Algolia.",
     link: "https://keywords.selfstudy.app/",
@@ -48,7 +56,7 @@ const items = [
     client: "",
     description:
       "An online jobboard for remote development & design professionals of all stripes and colors.",
-    img: "",
+    img: <TestImage />,
     webstack:
       "React, Django Rest Framework, Netlify, Heroku, REST API, and ECMAScript6",
     link: "https://www.selards.com",
@@ -63,42 +71,50 @@ const Gallery = props => {
         <h1 className=" text-light header-shadow text-underline">My Work</h1>
       </Link>
       <div className="gallery">
-        <div className="row">
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+          spacing={6}
+        >
+          {" "}
           {items.map(item => (
-            <div className="gallery-item double-column">
-              <div className="gallery-image"></div>
-              <div className="gallery-header">
-                {item.img}
-                <h1> {item.title}</h1>
-              </div>
-              <div className="gallery-header-sub">{item.client}</div>
-              <div className="gallery-description">{item.description}</div>
-              <div className="gallery-webstack">
-                {item.webstack ? (
-                  <b>
-                    Webstack: <br />
-                  </b>
-                ) : (
-                  <> </>
-                )}
-                {item.webstack}
-                <div className="gallery-link">
-                  {item.link ? (
-                    <a href={item.link} className="normalize">
-                      <button className="button-gallery">VIEW SITE</button>
-                    </a>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <div className="gallery-item">
+                <div className="gallery-image"> {item.img}</div>
+                <div className="gallery-header">
+                  <h1> {item.title}</h1>
+                </div>
+                <div className="gallery-header-sub">{item.client}</div>
+                <div className="gallery-description">{item.description}</div>
+                <div className="gallery-webstack">
+                  {item.webstack ? (
+                    <b>
+                      Webstack: <br />
+                    </b>
                   ) : (
-                    <div style={{ color: "grey" }}>
-                      {" "}
-                      This site is behind an auth layer and is not available for
-                      viewing
-                    </div>
+                    <> </>
                   )}
+                  {item.webstack}
+                  <div className="gallery-link">
+                    {item.link ? (
+                      <a href={item.link} className="normalize">
+                        <button className="button-gallery">VIEW SITE</button>
+                      </a>
+                    ) : (
+                      <div style={{ color: "grey" }}>
+                        {" "}
+                        This site is behind an auth layer and is not available
+                        for viewing
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
+            </Grid>
           ))}
-        </div>
+        </Grid>
       </div>
     </>
   )
