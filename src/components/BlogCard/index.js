6d145f62d424ddee67a3c8b0c0ce69374cea03a9
eathren <nolan.braman@gmail.com>
2020-02-20 09:26:58 -0500

    fixed margins

--- src/components/BlogCard/index.js
+++ src/components/BlogCard/index.js
@@ -34,45 +34,47 @@ export default () => (
     `}
     render={data => (
       <segment title="Blog Bar">
-        <div style={{}} className="blog-card ">
-          {/* <Link to="/blog">
-            <h3 className="blog-card-title flex-center"> Latest Posts</h3>
-            <hr />
-          </Link> */}
+        <div class="blog-card-container">
+          <div style={{}} className="blog-card ">
+            {/* <Link to="/blog">
+        <h3 className="blog-card-title flex-center"> Latest Posts</h3>
+        <hr />
+        </Link> */}
 
-          {data.allMarkdownRemark.edges.map(({ node }) => {
-            const title = node.frontmatter.title || node.fields.slug
-            return (
-              <div key={node.fields.slug}>
-                <Link className="" to={node.fields.slug}>
-                  <h2 className="blog-card-title">{title}</h2>
-                </Link>
-                <small className="blog-card-date">
-                  {node.frontmatter.date} - {node.timeToRead} minutes
-                </small>
-                <p
-                  style={{ marginBottom: "0" }}
-                  dangerouslySetInnerHTML={{
-                    __html: node.frontmatter.description || node.excerpt,
-                  }}
-                />
-                <br />
-                <Link className="" to={node.fields.slug}>
-                  <button className="button-blog">Read More</button>
-                </Link>
-                <br />
-                <br />
-                <hr
-                  style={{
-                    textAlign: "center",
-                    margin: "auto",
-                    width: "95%",
-                  }}
-                ></hr>
-                <br />
-              </div>
-            )
-          })}
+            {data.allMarkdownRemark.edges.map(({ node }) => {
+              const title = node.frontmatter.title || node.fields.slug
+              return (
+                <div key={node.fields.slug}>
+                  <Link className="" to={node.fields.slug}>
+                    <h2 className="blog-card-title">{title}</h2>
+                  </Link>
+                  <small className="blog-card-date">
+                    {node.frontmatter.date} - {node.timeToRead} minutes
+                  </small>
+                  <p
+                    style={{ marginBottom: "0" }}
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                  />
+                  <br />
+                  <Link className="" to={node.fields.slug}>
+                    <button className="button-blog">Read More</button>
+                  </Link>
+                  <br />
+                  <br />
+                  <hr
+                    style={{
+                      textAlign: "center",
+                      margin: "auto",
+                      width: "95%",
+                    }}
+                  ></hr>
+                  <br />
+                </div>
+              )
+            })}
+          </div>
         </div>
       </segment>
     )}
